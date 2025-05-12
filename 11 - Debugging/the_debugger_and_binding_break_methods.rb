9a05ee279101af49b01a0c8d617f370cc74a4b4c
Praveen2315 <praveenmathew2315@gmail.com>
2025-05-12 18:28:49 +0530

    ruby debugger

---
+++ 11 - Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,14 @@
+require 'debug'
+candy = "cotton candy"
+
+puts "i love candy: #{candy}"
+
+binding.break
+
+candy2 = "cotton candy2"  
+
+puts "i don't like candy2: #{candy2}"
+
+binding.break
+
+puts 'end of the program'
---
+++ 11 - Debugging/the_step_keyword.rb
@@ -0,0 +1,7 @@
+require "debug"
+
+debugger
+
+puts "good news".upcase
+
+puts "BAD NEWS".downcase
